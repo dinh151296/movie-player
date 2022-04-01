469ecc04faaf760cf1f7250a82061d7a01b448a0
ThanhDinh <mr.thanhdinh1996@gmail.com>
2022-04-02 02:01:52 +0700

    first-cm

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+// menu left
+let iconMenuElement = document.querySelector('.header-menu-icon');
+iconMenuElement.addEventListener('click', function(){
+    document.querySelector('.wrap-content-page').classList.toggle('hide-show-aside');
+});
+
+
